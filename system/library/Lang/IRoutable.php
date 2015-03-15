5b9f7eb6a2a95058ff6ceef3110d83563776b107
Allen Linatoc <linatoc.allen@programmer.net>
2015-03-15 23:24:12 +0800

    Added IRoutable @ \POPS\Lang

---
+++ system/library/Lang/IRoutable.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Made under PHPOps Open-Source Non-proprietary Project
+ *
+ * Pinoy Programmers League
+ */
+
+namespace POPS\Lang;
+
+/**
+ * Interface for routable controllers
+ *
+ * @author Allen
+ */
+interface IRoutable {
+
+    function getRoute();
+
+}
