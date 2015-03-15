afb7124ff445fc1cf1e340fa99ff81c51bfb463e
Allen Linatoc <linatoc.allen@programmer.net>
2015-03-15 22:20:06 +0800

    Added datatype "Boolean"

---
+++ system/library/Types/Boolean.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * Made under PHPOps Open-Source Non-proprietary Project
+ *
+ * Pinoy Programmers League
+ */
+
+namespace POPS\Types;
+
+/**
+ * Strongly-typed datatype that represents boolean
+ *
+ * @author Allen
+ */
+class Boolean extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype {
+
+    private $value;
+
+    public function __construct($value = NULL) {
+        if (!is_bool($value)) {
+            throw new \POPS\Exceptions\InvalidBooleanException($value);
+        }
+        $this->value = $value;
+    }
+
+    public function toString() {
+        return strval($this);
+    }
+
+    public function __toString() {
+        return $this->value ? "true" : "false";
+    }
+
+}
