ffbcb24b69595c13afed99c683c62ae682f8b949
Allen Linatoc <linatoc.allen@programmer.net>
2015-03-18 11:35:53 +0800

    Overriden "isValid()" method

--- system/library/Types/String.php
+++ system/library/Types/String.php
@@ -11,8 +11,9 @@ namespace POPS\Types;
 /**
  * Datatype for string values
  */
-class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype {
+class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatypeGeneric {
 
+    private $value;
 
 
     /**
@@ -21,7 +22,8 @@ class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype
      * @param string $value The string value to be contained in this String object
      * @throws InvalidStringException   Thrown when an invalid string value was provided
      */
-    public function __construct($value=NULL) {
+    public function __construct($value=NULL)
+    {
         if ($value!==NULL && !is_string($value)) {
             throw new InvalidStringException();
         }
@@ -39,7 +41,10 @@ class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype
      */
     public function &append($str)
     {
-        $this->setValue($this->getValue().$str);
+        if (!$this->isValid($str, true)) {
+            throw new \POPS\Exceptions\InvalidStringException();
+        }
+        $this->setValue($this->getValue() . $str);
         RETURN $this;
     }
 
@@ -50,6 +55,7 @@ class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype
      *
      * @param int $start If start is NON-NEGATIVE, the returned string will start at the start'th position in string, counting from zero. For instance, in the string 'abcdef', the character at position 0 is 'a', the character at position 2 is 'c', and so forth. If start is NEGATIVE, the returned string will start at the start'th character from the end of string. If string is LESS THAN OR EQUAL to start characters long, FALSE will be returned.
      * @param int $length {=NULL} If length is given and is positive, the string returned will contain at most length characters beginning from start (depending on the length of string). If length is given and is negative, then that many characters will be omitted from the end of string (after the start position has been calculated when a start is negative). If start denotes the position of this truncation or beyond, false will be returned. If length is given and is 0, FALSE or NULL an empty string will be returned. If length is omitted, the substring starting from start until the end of the string will be returned.
+     * 
      * @return string The extracted part of string, or FALSE on failure or empty string value
      */
     public function subString($start, $length=NULL)
@@ -100,6 +106,25 @@ class String extends \POPS\Lang\AbstractDatatype implements \POPS\Lang\IDatatype
     {
         $this->setValue('');
         RETURN $this;
+    }
+
+
+    /**
+     * Check if current value is valid, if a new value is specified, then such provided value will be tested instead, depending on the boolean setting of second parameter
+     *
+     * @param string    $newvalue {=null} New value to be tested and will also replace existing value of this String instance
+     * @param boolean   $is_noassign {=false} If value should be assigned to current instance's value
+     *
+     * @return boolean
+     */
+    public function isValid($newvalue=null, $is_noassign=false)
+    {
+        $this->value = $newvalue===null ? $this->value : ($is_noassign ? $this->value : $newvalue);
+        if ($newvalue!==null) {
+            if (!is_string($newvalue)) {
+                return false;
+            }
+        }
     }
 
 
